0c8698ba139c004445607b5798daa9a86b1f51b2
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-06-07 14:59:21 +0200

    sav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MAT_DATE_LOCALE,
+  MAT_DATE_FORMATS} from '@angular/material';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  @NgModule({
+    declarations: [
+      AppComponent
+    ],
+    imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatIconModule,
+      MatCheckboxModule,
+      MatInputModule,
+      MatNativeDateModule,
+      MatDatepickerModule
+    ],
+    providers: [{provide: MAT_DATE_LOCALE, useValue: 'fr-FR'}, ],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
